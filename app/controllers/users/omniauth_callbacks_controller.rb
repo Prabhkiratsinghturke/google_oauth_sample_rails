d8b52bfbd52653c5cb9aae2b291291053643f113
prabhkirat-s <prabhkirat.singh@bacancy.com>
2024-01-16 20:46:06 +0530

    Added google oauth with devise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -21,10 +21,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super
   # end
 
+  def google_oauth2
+    user = User.from_omniauth(auth)
+    if user.present?
+      sign_out_all_scopes
+      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} in not confirmed"
+    end
+  end
+
   # protected
 
   # The path used when OmniAuth fails
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
+  private
+
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token(20)
+      user.full_name = auth.info.name
+      user.avatar_url = auth.info.image
+
+      #if you are using confirmatble and the provider(s) you use validate emails.
+      #uncomment below line to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
